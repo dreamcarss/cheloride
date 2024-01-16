9f839a650cb1bf234b41272db406b522cce82d07
programmer141-dev <sidhardhchandra141@gmail.com>
2024-01-16 11:48:53 +0530

    exp

--- models/tempbooking.js
+++ models/tempbooking.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose")
 const tempBookingSchema = mongoose.Schema({
   data: { type: Array, require: true },
   carId: { type: String, require: true },
-  userId: { type: String, require: true }
+  userId: { type: String, require: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600,
+  }
 });
 
 const tempBooking = mongoose.model("tempBookings", tempBookingSchema);
