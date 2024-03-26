271880e6e65425513e467c29162462b249c13f2c
programmer-141 <sidhardhchandra141@gmail.com>
2024-03-26 12:04:24 +0530

    exp time

--- models/trashbin.js
+++ models/trashbin.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const trashSchema = mongoose.Schema({
-    trash: {type: Object, require: true}
+    trash: {type: Object, require: true},
+    createdAt: { type: Date, expires: 3600 }
 })
 
 const trashModel = mongoose.model("trash", trashSchema);
